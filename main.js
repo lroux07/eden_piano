b35cba18fb90628e0410eee36430907e06872dce
lroux07 <lroux07@edenschool.fr>
2024-04-11 17:11:02 +0200

    Add clavier

---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+let octavePatern = 'BNBNBBNBNBNB';
+let octaves_count = 2;
+
+for(let i = 0; i < octaves_count; i++) {
+    for(let j = 0; j < octavePatern.length; j++) {
+        if(octavePatern[j] + octavePatern[j + 1] === 'BN') {
+            const white = document.createElement('div');
+            white.className = 'white';
+            container.appendChild(white);
+    
+            const black = document.createElement('div');
+            black.className = 'black';
+            white.appendChild(black);
+        } else if(octavePatern[j] + octavePatern[j + 1] === 'BB') {
+            const white = document.createElement('div');
+            white.className = 'white';
+            container.appendChild(white);
+        }
+    }
+
+    const white = document.createElement('div');
+    white.className = 'white';
+    container.appendChild(white);
+}
